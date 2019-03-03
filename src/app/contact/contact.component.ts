81c3ff5336a610ec23bcb66381d6aea6612406f4
Bill Horst <bill@billhorst.com>
2019-03-02 20:32:03 -0700

    commented out all contact form code until adding formspree

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,24 +13,24 @@ export class ContactComponent implements OnInit {
   private language: string = this.languageService.getLanguage();
   languageSubscription: any;
 
-  private namePlaceholder: string;
-  private emailPlaceholder: string;
-  private notesPlaceholder: string;
-  private submitButtonValue: string;
-  private nameHint: string;
-  private nameError: string;
-  private emailError: string;
+  // namePlaceholder: string;
+  // emailPlaceholder: string;
+  // notesPlaceholder: string;
+  // submitButtonValue: string;
+  // nameHint: string;
+  // nameError: string;
+  // emailError: string;
 
-  contactForm: FormGroup;
+  // contactForm: FormGroup;
 
   constructor(private router: Router, private languageService: LanguageService) { }
 
   ngOnInit() {
-    this.contactForm = new FormGroup({
-      userName: new FormControl('', {validators: [Validators.required]}),
-      userEmail: new FormControl('', {validators: [Validators.required, Validators.email]}),
-      userNotes: new FormControl('', {validators: []})
-    });
+    // this.contactForm = new FormGroup({
+    //   userName: new FormControl('', {validators: [Validators.required]}),
+    //   userEmail: new FormControl('', {validators: [Validators.required, Validators.email]}),
+    //   userNotes: new FormControl('', {validators: []})
+    // });
     this.languageSubscription = this.languageService.triggerLanguageChange.subscribe((language: string) => {
       this.setTranslations(language);
     });
@@ -38,40 +38,39 @@ export class ContactComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.contactForm);
-    this.router.navigate(['/thankyou']);
+    // this.router.navigate(['/thankyou']);
   }
 
   setTranslations(language: string) {
     let langTrans = this.translations[language];
 
-    this.namePlaceholder = langTrans.namePlaceholder;
-    this.emailPlaceholder = langTrans.emailPlaceholder;
-    this.notesPlaceholder = langTrans.notesPlaceholder;
-    this.submitButtonValue = langTrans.submitButtonValue;
-    this.nameHint = langTrans.nameHint;
-    this.nameError = langTrans.nameError;
-    this.emailError = langTrans.emailError;
+    // this.namePlaceholder = langTrans.namePlaceholder;
+    // this.emailPlaceholder = langTrans.emailPlaceholder;
+    // this.notesPlaceholder = langTrans.notesPlaceholder;
+    // this.submitButtonValue = langTrans.submitButtonValue;
+    // this.nameHint = langTrans.nameHint;
+    // this.nameError = langTrans.nameError;
+    // this.emailError = langTrans.emailError;
   }
 
   private translations: {} = {
     english: {
-      namePlaceholder: 'Full name',
-      emailPlaceholder: 'Email address',
-      notesPlaceholder: 'Notes',
-      submitButtonValue: 'Submit',
-      nameHint: '(You or your company\'s name)',
-      nameError: 'Missing name',
-      emailError: 'Missing or invalid email'
+      // namePlaceholder: 'Full name',
+      // emailPlaceholder: 'Email address',
+      // notesPlaceholder: 'Notes',
+      // submitButtonValue: 'Submit',
+      // nameHint: '(You or your company\'s name)',
+      // nameError: 'Missing name',
+      // emailError: 'Missing or invalid email'
     },
     japanese: {
-      namePlaceholder: 'お名前',
-      emailPlaceholder: 'メール',
-      notesPlaceholder: 'ノート',
-      submitButtonValue:　'ゴー',
-      nameHint: 'お前か会社の名前',
-      nameError: '名前ねーっし',
-      emailError: 'メールないかメールめちゃくちゃだ'
+      // namePlaceholder: 'お名前',
+      // emailPlaceholder: 'メール',
+      // notesPlaceholder: 'ノート',
+      // submitButtonValue:　'ゴー',
+      // nameHint: 'お前か会社の名前',
+      // nameError: '名前ねーっし',
+      // emailError: 'メールないかメールめちゃくちゃだ'
     }
   }
 
